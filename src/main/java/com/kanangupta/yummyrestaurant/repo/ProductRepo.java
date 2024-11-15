82700022e3e755152e0ace76d7e88f8a99c274cd
Kanan-30 <kanan30082002@gmail.com>
2024-11-15 20:23:44 +0530

    Added Products Entity and performed CRUD operations and range query

---
+++ src/main/java/com/kanangupta/yummyrestaurant/repo/ProductRepo.java
@@ -0,0 +1,14 @@
+package com.kanangupta.yummyrestaurant.repo;
+import com.kanangupta.yummyrestaurant.entity.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.price BETWEEN :minPrice AND :maxPrice ORDER BY p.price ASC")
+    List<Product> findTop2ByPriceBetweenOrderByPriceAsc(@Param("minPrice") double minPrice, @Param("maxPrice") double maxPrice);
+}
